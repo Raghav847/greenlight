fb3db58bdee04167c4817462c192acd57d6a8a3f
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-05-27 20:13:17 -0700

    struct tags

---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`                //unique int ID for the movie
+	CreatedAt time.Time `json:"-"`                 //time for when added to DB
+	Title     string    `json:"title"`             //movie title
+	Year      int32     `json:"year,omitempty"`    //movie year
+	Runtime   int32     `json:"runtime,omitempty"` //movie runtime
+	Genres    []string  `json:"genres,omitempty"`  //slices of genres
+	Version   int32     `json:"version"`           //increment when updated
+}
