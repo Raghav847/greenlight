97a9c09cd702081bdde7fdfdc845b531202415ef
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-06-03 19:08:25 -0700

    data validation

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -66,7 +66,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 
 		case errors.As(err, &unmarshalTypeError):
 			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf("body contains incorrect JSON type for feild %q", unmarshalTypeError.Field)
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
 
